adc8867fe699e93f89daa24ec0d28ba705bb8fca
Yuto Sagawa <yuto.sagawa@persol.co.jp>
2020-01-30 17:51:00 +0900

    add: meta tags

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,11 @@ import App, { AppContext } from 'next/app';
 import { GlobalStyle } from '../assets/style/globalStyle';
 import { Provider } from 'react-redux';
 import { Store } from '../store/';
+import Head from 'next/head';
+
+const title = 'Next.js Sample';
+const description = 'This is a dscription.';
+const keywords = ['転職サイト', '求人'];
 
 export default class extends App {
   static async getInitialProps({ Component, ctx }: AppContext) {
@@ -19,6 +24,21 @@ export default class extends App {
     const { Component, pageProps } = this.props;
     return (
       <>
+        <Head>
+          <meta charSet="utf-8" />
+          <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+          <meta name="robots" content="index, follow" />
+          <title>{title}</title>
+          <meta name="description" content={description} />
+          <meta name="keywords" content={keywords.join(',')} />
+          <meta name="format-detection" content="telephone=no" />
+          <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+          <meta property="og:type" content="article" />
+          <meta property="og:title" content={title} />
+          <meta property="og:description" content={description} />
+          <meta property="og:site_name" content={title} />
+          <meta property="og:locale" content="ja_JP" />
+        </Head>
         <Provider store={Store}>
           <Component {...pageProps} />
           <GlobalStyle />
